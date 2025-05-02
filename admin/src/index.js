029883c3926531104962f60cfd351a36b3766af3
TestHabitatPlush <hptestplatform@gmail.com>
2025-05-02 12:52:34 +0000

    Fresh

---
+++ admin/src/index.js
@@ -0,0 +1,23 @@
+  import React from "react";
+  import ReactDOM from "react-dom/client";
+  import "./index.css";
+  import App from "./App";
+  import reportWebVitals from "./reportWebVitals";
+  import { BrowserRouter as Router } from "react-router-dom";
+  import { Provider } from "react-redux";
+  import store from "./redux/store";
+  import { Toaster } from "react-hot-toast";
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        {/* Added this line */}
+        <Router basename="/admin">
+          <Toaster />
+          <App />
+        </Router>
+      </Provider>
+    </React.StrictMode>
+  );
+  reportWebVitals();
